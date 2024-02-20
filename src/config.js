9cb822189c3864dad7f76cc76b571794c1eba7b4
Sandesh7969 <155588072+Sandesh7969@users.noreply.github.com>
2024-02-20 11:55:30 +0545

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-  TOKEN: "",
+  TOKEN: "" || process.env['TOKEN'],
   MONGO: "mongodb+srv://Jarvis:nitrixexe@cluster0.bxzzf70.mongodb.net/?retryWrites=true&w=majority",
   OWNER: ["1131953433134497923"],
   PREFIX: "+",
-  WEBHOOK: "https://discord.com/api/webhooks/1198831493523451936/9544EhyZzJK1sssOl9o4ZExaahDx7PCzeDBSPCNu3nGMFUeasfDwunwr1lAl8-8jWOud",
+  WEBHOOK: "https://discord.com/api/webhooks/1206509224796880906/ac0KbKQv5FmoUaLVijQmKRO_mUtb0Dg8hLwFyeGuBePlhc6rB-i5l2Rj87xV8Z1VUBHR",
   COLOR: "#1e5eb9",
   EMOJIS: {
     TICK: "<a:tick_kastro:1191703538485702727>",
